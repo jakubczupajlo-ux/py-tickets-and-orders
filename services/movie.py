39a1a5ae30c3160beef6c8018fbc7dbf529ae464
Jakub <jakubczupajlo@gmail.com>
2026-03-23 13:47:19 +0100

    tickets

--- services/movie.py
+++ services/movie.py
@@ -1,11 +1,12 @@
+from django.db import transaction
 from django.db.models import QuerySet
-
 from db.models import Movie
 
 
 def get_movies(
     genres_ids: list[int] = None,
     actors_ids: list[int] = None,
+    title: str = None,  # Dodany opcjonalny argument
 ) -> QuerySet:
     queryset = Movie.objects.all()
 
@@ -15,6 +16,9 @@ def get_movies(
     if actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
 
+    if title:  # Filtrowanie po tytule (icontains - wielkość liter nie ma znaczenia)
+        queryset = queryset.filter(title__icontains=title)
+
     return queryset
 
 
@@ -22,6 +26,7 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
+@transaction.atomic  # Dodany dekorator transakcji
 def create_movie(
     movie_title: str,
     movie_description: str,
---
+++ services/order.py
@@ -0,0 +1,26 @@
+from django.db import transaction
+from django.contrib.auth import get_user_model
+from db.models import Order, Ticket
+
+User = get_user_model()
+
+@transaction.atomic
+def create_order(tickets, username, date=None):
+    user = User.objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = date
+        order.save()
+    for t_data in tickets:
+        Ticket.objects.create(
+            movie_session_id=t_data["movie_session"],
+            order=order,
+            row=t_data["row"],
+            seat=t_data["seat"]
+        )
+
+def get_orders(username=None):
+    queryset = Order.objects.all()
+    if username:
+        queryset = queryset.filter(user__username=username)
+    return queryset
